ecdf641ebf0e2bdaa818a6b6e5d9b043ce093375
vovakutsanov <kutsanov@mail.ru>
2020-02-29 01:06:14 +0700

    Initial Commit

---
+++ lesson_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson_2
+//
+//  Created by Vova Kutsanov on 29.02.2020.
+//  Copyright © 2020 Vova Kutsanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
